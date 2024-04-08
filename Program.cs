9053883edbb722a4ae05ed1c6ae9ece774d05bc4
arthurduzhyy <163334544+arthurduzhyy@users.noreply.github.com>
2024-04-08 15:15:32 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 ﻿namespace AbstractFactoryProject;
 
+// Системи майнінгу (GPU, ASIC, CPU, FPGA): Створити систему
+// для вибору обладнання для майнінгу криптовалют залежно
+// від алгоритму майнінгу, що максимізує прибутковість.
+
 public class Program
 {
     public static void Main(string[] args)
@@ -8,87 +12,79 @@ public class Program
     }
 }
 
-public interface IAbstractFactory
+public interface IMining
 {
-    IAbstractProductA CreateProductA();
-    IAbstractProductB CreateProductB();
+    void Mine();
 }
 
-public class ConcreteFactory1 : IAbstractFactory
+public class GPUMining : IMining
 {
-    public IAbstractProductA CreateProductA() => new ConcreteProductA1();
-
-    public IAbstractProductB CreateProductB() => new ConcreteProductB1();
+    public void Mine() => Console.WriteLine("Mining with GPU!");
 }
 
-public class ConcreteFactory2 : IAbstractFactory
+public class ASICMining : IMining
 {
-    public IAbstractProductA CreateProductA() => new ConcreteProductA2();
-
-    public IAbstractProductB CreateProductB() => new ConcreteProductB2();
+    public void Mine() => Console.WriteLine("Mining with ASIC!");
 }
 
-public interface IAbstractProductA
+public class CPUMining : IMining
 {
-    string UsefulFunctionA();
+    public void Mine() => Console.WriteLine("Mining with CPU!");
 }
 
-public class ConcreteProductA1 : IAbstractProductA
+public class FPGAMining : IMining
 {
-    public string UsefulFunctionA() => "The result of the product A1.";
+    public void Mine() => Console.WriteLine("Mining with FPGA!");
 }
 
-public class ConcreteProductA2 : IAbstractProductA
+public interface IMiningFactory
 {
-    public string UsefulFunctionA() => "The result of the product A2.";
+    IMining CreateMining();
 }
 
-public interface IAbstractProductB
+public class GPUMiningFactory : IMiningFactory
 {
-    string UsefulFunctionB();
-    string AnotherUsefulFunctionB(IAbstractProductA collaborator);
+    public IMining CreateMining() => new GPUMining();
 }
 
-public class ConcreteProductB1 : IAbstractProductB
+public class ASICMiningFactory : IMiningFactory
 {
-    public string UsefulFunctionB() => "The result of the product B1.";
-
-    public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
-    {
-        var result = collaborator.UsefulFunctionA();
-        return $"The result of the B1 collaborating with the ({result})";
-    }
+    public IMining CreateMining() => new ASICMining();
 }
 
-public class ConcreteProductB2 : IAbstractProductB
+public class CPUMiningFactory : IMiningFactory
 {
-    public string UsefulFunctionB() => "The result of the product B2.";
+    public IMining CreateMining() => new CPUMining();
+}
 
-    public string AnotherUsefulFunctionB(IAbstractProductA collaborator)
-    {
-        var result = collaborator.UsefulFunctionA();
-        return $"The result of the B2 collaborating with the ({result})";
-    }
+public class FPGAMiningFactory : IMiningFactory
+{
+    public IMining CreateMining() => new FPGAMining();
 }
 
 public class Client
 {
     public void Main()
     {
-        Console.WriteLine("Client: Testing client code with the first factory type...");
-        ClientMethod(new ConcreteFactory1());
-        Console.WriteLine();
+        var factories = new List<IMiningFactory>()
+        {
+            new GPUMiningFactory(),
+            new ASICMiningFactory(),
+            new CPUMiningFactory(),
+            new FPGAMiningFactory()
+        };
 
-        Console.WriteLine("Client: Testing the same client code with the second factory type...");
-        ClientMethod(new ConcreteFactory2());
+        foreach (var item in factories)
+        {
+            Console.WriteLine($"Client: Testing client code with the {item.GetType().Name} factory type...");
+            ClientMethod(item);
+            Console.WriteLine();
+        }
     }
 
-    public void ClientMethod(IAbstractFactory factory)
+    private void ClientMethod(IMiningFactory factory)
     {
-        var productA = factory.CreateProductA();
-        var productB = factory.CreateProductB();
-
-        Console.WriteLine(productB.UsefulFunctionB());
-        Console.WriteLine(productB.AnotherUsefulFunctionB(productA));
+        var f = factory.CreateMining();
+        f.Mine();
     }
 }
